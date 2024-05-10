176fe66efa821783b8c460d8065b9a057ffea49a
magnificmemories <sauracompany@gmail.com>
2024-05-10 17:41:20 -0600

    Terminando backend

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { login, register } from '../controllers/auth.controller.js'
-
+import { login, logout, register, profile } from '../controllers/auth.controller.js'
+import { authRequired } from '../middlewares/validateToken.js';
 const router = Router()
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { loginSchema } from '../schemas/auth.schema.js';
+import { registerSchema } from '../schemas/auth.schema.js';
 
-router.post('/register', register);
-router.post('/login', login)
-
+router.post('/register', validateSchema(registerSchema), register);
+router.post('/login', validateSchema(loginSchema), login)
+router.post('/logout', logout)
+router.get("/profile", authRequired, profile)
 export default router;
